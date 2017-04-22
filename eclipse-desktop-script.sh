7f93f0622a49bb0e1174030c6ac554eb2395e1e3
iacosite <iacopo.olivo@gmail.com>
2017-04-22 15:59:30 +0200

    Update eclipse-desktop-script.sh

--- eclipse-desktop-script.sh
+++ eclipse-desktop-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 path=/usr/share/applications/eclipse.desktop
 
-if [ $(id -u)  -gt 0 ]; then
+if [ "$(id -u)" != "0" ]; then
 	echo "Super user privileges required, use sudo"
 	exit
 fi
@@ -10,13 +10,17 @@ echo "Insert entry name:"
 read name
 
 echo "Looking for eclipse executable..."
+
 #gather all the eclipse executables present in ~ and /opt/ (usual installation directories)
-read -a eclipse <<<$(find $HOME /opt -not -path '*\/\.local*' -type f -name eclipse -perm /u=x 2>/dev/null)
+read -r -a eclipse << get_executables
+$(find $HOME /opt -not -path '*\/\.local*' -type f -name eclipse -perm /u=x 2>/dev/null)
+get_executables
+
 
 #let user select the executable to create desktop shortcut
 if [ ${#eclipse[@]} -gt 1 ]; then
 	echo "Select the executable you want to create the desktop entry:"
-	select opt in ${eclipse[@]}; do
+	select opt in "${eclipse[@]}"; do
 		executable=$opt
 		break
 	done
@@ -29,23 +33,16 @@ if [ ! -x $executable ]; then
 fi
 
 #locate the icon file
-icon=$(find $(dirname $executable) -type f -name icon.xpm 2>/dev/null)
+icon=$(find "$(dirname $executable)" -type f -name icon.xpm 2>/dev/null)
 
 
 #create the desktop entry
-echo [Desktop Entry] > $path
-echo Type=Application >> $path
-echo Name=$name >> $path
-echo Comment=Eclipse Integrated Development Environment >> $path
+printf "[Desktop Entry]\nType=Application\nName=%s\nComment=Eclipse Integrated Development Environment\n" "$name" > $path
 if [ "$icon" == "" ];then
 	echo "Icon not found"
 else
-	echo Icon=$icon >> $path
+	echo "Icon=$icon" >> $path
 fi
-echo Exec=$executable >> $path
-echo Terminal=false >> $path
-echo Categories=Development\;IDE >> $path
-echo StartupWMClass=Eclipse >> $path
+printf "Exec=%s\nTerminal=false\nCategories=Development;IDE\nStartupWMClass=Eclipse\n" $executable >> $path
 
 echo "Desktop entry created with success! have fun"
-
